2ce9c252eeb41a3c5662e6d3ab5016383a6778e7
Sergio Villafan <villafan815@gmail.com>
2025-03-22 01:08:57 -0400

    Removiendo uso de sesion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,15 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Tasks from "./pages/Tasks";
-import useSession from "./store/authStore";
+import {useAuthStore} from "./store/authStore";
 
 function App() {
-  const { user, loading } = useSession();
-
-  if (loading) return <p>Cargando sesión...</p>;
+  const { token } = useAuthStore();
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to={user ? "/tasks" : "/login"} />} />
+        <Route path="/" element={<Navigate to={token ? "/tasks" : "/login"} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/tasks" element={<Tasks />} />
